fd46c9c2cf52e77adf0ad28e657c348c07b64711
Varad0910 <76674690+Varad0910@users.noreply.github.com>
2021-05-20 15:00:15 +0530

    Google Brand Studio Evaluation demo

---
+++ index.js
@@ -0,0 +1,18 @@
+function onHoverSubmenu1(){
+    document.getElementById("submenu1").style.display='block';
+    document.getElementById("racialequality").style.display='none';
+}
+function onOutSubmenu1(){
+    document.getElementById("submenu1").style.display='none';
+    document.getElementById("racialequality").style.display='block';
+}
+
+
+function onHoverSubmenu2(){
+    document.getElementById("submenu2").style.display='block';
+    document.getElementById("racialequality").style.display='none';
+}
+function onOutSubmenu2(){
+    document.getElementById("submenu2").style.display='none';
+    document.getElementById("racialequality").style.display='block';
+}
