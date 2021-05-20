91ccb3a83506956125a3bd72c3ad905768ef3e67
Varad0910 <76674690+Varad0910@users.noreply.github.com>
2021-05-20 15:10:29 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 function onHoverSubmenu1(){
-    document.getElementById("submenu1").style.display='block';
-    document.getElementById("racialequality").style.display='none';
+    document.getElementById("submenu1").style.display='block';    //hover for submenu1
+    document.getElementById("racialequality").style.display='none'; 
 }
 function onOutSubmenu1(){
-    document.getElementById("submenu1").style.display='none';
+    document.getElementById("submenu1").style.display='none';     
     document.getElementById("racialequality").style.display='block';
 }
 
 
 function onHoverSubmenu2(){
-    document.getElementById("submenu2").style.display='block';
+    document.getElementById("submenu2").style.display='block';        //hover for submenu2
     document.getElementById("racialequality").style.display='none';
 }
 function onOutSubmenu2(){
     document.getElementById("submenu2").style.display='none';
-    document.getElementById("racialequality").style.display='block';
+    document.getElementById("racialequality").style.display='block';  
 }
